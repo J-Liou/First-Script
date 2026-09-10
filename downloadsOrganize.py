8904ce4a26b63dacc4a895178a38a627e4fad349
J-Liou <jonathanliou0@gmail.com>
2026-09-09 23:49:01 -0500

    finish initial script

--- downloadsOrganize.py
+++ downloadsOrganize.py
@@ -3,6 +3,7 @@ import shutil
 from subprocess import PIPE
 from pathlib import Path
 import hashlib
+from send2trash import send2trash
 
 class hashedFile:
     def __init__(self, name, hash):
@@ -20,29 +21,52 @@ def fileHasher(file):
     return hasher.hexdigest()
 
 def dupeFiltering(folder):
-    files = {}
+    seen = {}
 
     for file in folder.iterdir():
         if file.is_file():
             hash = fileHasher(file)
-            if hash in files:
-                files[hash] = file
-                print(f"{file} file deleted")
+            if hash in seen: #file already in set
+                send2trash(file)
+                print(f"{file} file moved to trash")
+            else:
+                seen[hash] = file #add file to seen set
 
 
-
-def getDownloadsFolder():
-    return Path.home() / "Downloads"
 
 def createNewFolder(parentFolder, name: str):
-    return parentFolder / name
-    
+    folderPath = parentFolder / name
+    folderPath.mkdir(exists_ok=True)
+
+def createFolders(parentFolder, folderDict):
+    for key in folderDict.keys():
+        createNewFolder(parentFolder, key) #create folder with type name
+
+def mvFile2Folder(file, folderDict):
+    ext = Path(file).suffix.lower()
+    for folderName, extensions in folderDict.items():
+        if ext and ext in extensions: #file suffix aligns with a organized folder
+            destFolder = file.parent / folderName
+            shutil.move(file, destFolder)
+    else:
+        shutil.move(file, file.parent / 'Misc')
 
 def main():
-    downloadsFolder = getDownloadsFolder()
-    createNewFolder(downloadsFolder, 'Images')
-    for item in downloadsFolder.iterdir():
-        pass
+    folderTypes = {
+        'Images': ['.png', '.jpg', '.jpeg'],
+        'Files': ['.pdf', '.docx'],
+        'Executables': ['.exe'],
+        'Misc': []
+    }
+    downloads = Path.home() / "Downloads"
+    createFolders(downloads, folderTypes)
+
+    dupeFiltering(downloads)
+
+    for item in downloads.iterdir():
+        if item.is_file():
+            mvFile2Folder(item, folderTypes)
+
 
 if __name__ == "__main__":
     main()
